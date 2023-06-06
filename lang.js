dcf3aecc093d1a188b3499cb045c157b0fe242f9
ZEN-SMS <135694111+ZEN-SMS@users.noreply.github.com>
2023-06-06 10:33:57 +0200

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,29 @@
+langEN = {
+	"title_question" : "Do you remember any grocery stores that closed in Trondheim ?",
+	"title_instructions" : "Click on the map where you think a grocery stores was located but has since closed. Fill in the information and then press the \"Add a store\" button. A new page with a submission confirmation will open but you can close it and feel free to add more stores.",
+	"dispOpen" : "Display open stores",
+	"dispClosed" : "Display closed stores",
+	"hideOpen" : "Hide open stores",
+	"hideClosed" : "Hide closed stores",
+	"supermarket" : "Supermarket",
+	"kiosk" : "Kiosk",
+	"specialised" : "Specialised",
+	"closed_supermarket" : "Closed supermarket",
+	"closed_kiosk" : "Closed kiosk",
+	"closed_specialised" : "Closed specialised",
+};
+
+langNO = {
+	"title_question" : "Husker du noen dagligvarebutikker som stengte i Trondheim?",
+	"title_instructions" : "Klikk på kartet der du tror en dagligvarebutikk lå, men som siden har stengt. Fyll ut informasjonen og trykk deretter på knappen \"Legg til en butikk\". En ny side åpnes. Du kan lukke den og gjerne legge til flere butikker.",
+	"dispOpen" : "Vis åpne butikker",
+	"dispClosed" : "Vis stengte butikker",
+	"hideOpen" : "Skjuler åpne butikker",
+	"hideClosed" : "Skjuler stengte butikker",
+	"supermarket" : "Supermarked",
+	"kiosk" : "Kiosk",
+	"specialised" : "Spesialforretning",
+	"closed_supermarket" : "Stengt  Supermarked",
+	"closed_kiosk" : "Stengt  kiosk",
+	"closed_specialised" : "Stengt  Spesialforretning",
+};
